bee8f5dec940b1df604e8aa14560567c9b3e5412
Saumya Shah <47073679+saumyadshah@users.noreply.github.com>
2023-09-26 07:24:33 +0000

    feat - 02_dataTypes.js : Understanding of dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // Treat all Js code as the newer version of JS
+
+let name = "Saumya";
+let age = 22;
+let isLoggedIn = false;
+
+// number => Range - 2^53
+// bigint
+// string => ""
+// boolean => true/false
+// null => Standalone Value
+// undefined => Value not yet assigned
+// symbol => used for uniqueness
+
+// object
+
+console.log(typeof "Saumya");
+console.log(typeof 2);
+console.log(typeof false);
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
