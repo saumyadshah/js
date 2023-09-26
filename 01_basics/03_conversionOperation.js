70ff8acc68f7ca65715ca8e815e19f3ec3625c13
Saumya Shah <47073679+saumyadshah@users.noreply.github.com>
2023-09-26 07:59:51 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,16 @@
+let score = "33abc";
+
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+console.log(valueInNumber);
+console.log(typeof valueInNumber);
+
+/* 
+    "33" => 33
+    "33abc" => NaN
+    null => 0
+    undefined => NaN
+    true => 1
+    false => 0
+*/
