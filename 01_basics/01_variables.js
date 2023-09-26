d09dc2a3fcafc1b1b6e9d10be5b4c38f051f1815
Saumya Shah <47073679+saumyadshah@users.noreply.github.com>
2023-09-26 07:24:11 +0000

    feat - 01_variables.js : Understanding of variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144234;
+let accountEmail = "saumya@javascript.com";
+var accountPassword = "12344321";
+accountCity = "Ahmedabad";
+let accountState;
+
+// accountId = 2; // Not Allowed
+accountEmail = "ss@js.com";
+accountPassword = "12345"
+accountCity = "Bengaluru"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
